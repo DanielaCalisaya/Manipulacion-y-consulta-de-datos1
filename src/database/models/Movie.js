64fff2ab49bbee54423aadc5416f4d7ab82013b1
DanielaCalisaya <hdaniela127c@gmail.com>
2022-01-30 20:46:49 -0600

    codigo legible

--- src/database/models/Movie.js
+++ src/database/models/Movie.js
@@ -1,40 +1,40 @@
-module.exports = (sequelize, dataTypes) => {
+module.exports = (sequelize, dataTypes) => { //Necesito de estos dos objetos
 
     const alias = "Movie"; /* define como quiero llamar al modelo */
 
     const cols = {
-       id:{
-           type: dataTypes.INTEGER(10).UNSIGNED,
-           primaryKey: true,
-           autoIncrement: true,
-           allowNull: false
-       },
-       title: {
-           type: dataTypes.STRING(500),
-           allowNull: false
-       },
-       rating: {
-           type: dataTypes.DECIMAL(3,1).UNSIGNED,
-           allowNull: false
-       },
-       awards: {
-           type: dataTypes.INTEGER(10).UNSIGNED,
-           allowNull: false,
-           defaultValue: 0
-       },
-       release_date: { 
-           type: dataTypes.DATE,
-           allowNull: false
-       },
-       length: {
-           type: dataTypes.INTEGER(10).UNSIGNED,
-           defaultValue: null /* no hace falta */
-       },
-       genre_id: { 
-           type: dataTypes.INTEGER(10).UNSIGNED,
-           foreignKey: true, /* ---- */
-           defaultValue: null /* --- */
-       }
+        id: {
+            type: dataTypes.INTEGER(10).UNSIGNED,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        title: {
+            type: dataTypes.STRING(500),
+            allowNull: false
+        },
+        rating: {
+            type: dataTypes.DECIMAL(3,1).UNSIGNED,
+            allowNull: false
+        },
+        awards: {
+            type: dataTypes.INTEGER(10).UNSIGNED,
+            allowNull: false,
+            defaultValue: 0
+        },
+        release_date: { 
+            type: dataTypes.DATE,
+            allowNull: false
+        },
+        length: {
+            type: dataTypes.INTEGER(10).UNSIGNED,
+            defaultValue: null /* no hace falta */
+        },
+        genre_id: { 
+            type: dataTypes.INTEGER(10).UNSIGNED,
+            foreignKey: true, /* ---- */
+            defaultValue: null /* --- */
+        }
     };
 
     const config = {
